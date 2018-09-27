55173278bd18a1bf583ceed97007835c024a7c36
justincastilla <pxlperfection@gmail.com>
2018-09-27 13:59:44 -0700

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,12 +1,38 @@
 import React, { Component } from "react";
 import Header from "./Header";
+import CounterList from "./CounterList";
 
 class App extends Component {
+  constructor(){
+    super()
+    this.state = {
+      counters: 0
+    }
+  }
+
+  increaseCounters = () => {
+    let counters = this.state.counters + 1
+    this.setState({ counters })
+  }
+
+  decreaseCounters = () => {
+    let counters = this.state.counters - 1
+    this.setState({ counters })
+  }
+
+  // need to have numberOfCounters
   render() {
     return (
       <div className="App">
-        <Header />
+        <Header 
+          countersCount={this.state.counters}
+          increaseCounters={this.increaseCounters} 
+          decreaseCounters={this.decreaseCounters} />
+        <CounterList 
+          counters={this.state.counters} />
       </div>
     );
   }
 }
+
+export default App;
